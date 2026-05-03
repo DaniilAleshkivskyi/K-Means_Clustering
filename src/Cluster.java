50b422007b73afb3827473a2b15ab40d92df89b7
DaniilAleshkivskyi <milukovdaniil11@gmail.com>
2026-05-03 09:54:13 +0200

    testttt

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,4 +1,12 @@
 import java.util.List;
 
 public record Cluster(List<MyVector> centroids, List<MyVector> nearest) {
+    public Cluster(List<MyVector> centroids, List<MyVector> nearest) {
+        this.centroids = centroids;
+        this.nearest = nearest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("lalala");
+    }
 }
