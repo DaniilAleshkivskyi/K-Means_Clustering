a4e5853cd56fd88f32f46a9472e3c37adcd18463
DaniilAleshkivskyi <milukovdaniil11@gmail.com>
2026-05-03 09:54:49 +0200

    t

--- src/KMeans.java
+++ src/KMeans.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class KMeans {
     int k;
     List<Cluster> clusters;
+
+    public KMeans(int k) {
+        this.k = k;
+        clusters = new ArrayList<>();
+    }
 }
